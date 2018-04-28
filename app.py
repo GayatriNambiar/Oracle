3c5e1a9005ca75fe8a0ae514970b87ee7cce2e0c
GayatriNambiar <gnambiar22@gmail.com>
2018-04-28 13:37:14 -0400

    update remote access to cloud db

--- app.py
+++ app.py
@@ -4,13 +4,9 @@ from flask import Flask, render_template, redirect, request
 import html5lib
 import cx_Oracle
 import platform, sys
-# platform.architecture(), sys.maxsize
-#
-# print(sys.maxsize)
-#
-# print(platform.architecture())
-print("cx_Oracle version:", cx_Oracle.version)
-print("Oracle client version:", cx_Oracle.clientversion())
+
+# print("cx_Oracle version:", cx_Oracle.version)
+# print("Oracle client version:", cx_Oracle.clientversion())
 
 
 app = Flask(__name__)
@@ -22,23 +18,18 @@ application = Flask(__name__)
 # creating connection Object which will contain SQL Server Connection
 # connection = pypyodbc.connect('Driver={SQL Server};Server=.;Database=Student')  # Creating Cursor
 
-ip = '129.157.178.26'
+ip = '0.0.0.0'
 port = 1521
 SID = 'ORCL'
-dsn_tns = cx_Oracle.makedsn(ip, port, SID)
+service_name = 'servicename'
+dsn_tns = cx_Oracle.makedsn(ip, port, service_name=service_name)
 
-connection = cx_Oracle.connect('SYS', 'XzYVLwiD9_', dsn_tns)
+connection = cx_Oracle.connect(user="username", password="****", dsn=dsn_tns)
 
-# connection = cx_Oracle.connect(u'SYS/XzYVLwiD9_@Database20180427011028:1521/PDB1.595583066.oraclecloud.internal')
 
 cursor = connection.cursor()
 
-# s = "<table style='border:1px solid red'>"
-# for row in cursor:
-#     s = s + "<tr>"
-#     for x in row:
-#         s = s + "<td>" + str(x) + "</td>"
-#     s = s + "</tr>"
+
 
 @app.route('/')
 def index():
@@ -46,7 +37,8 @@ def index():
 
 @app.route('/tables')
 def table():
-    cur = cursor.execute("SELECT top(50) Pstatus,Medu,Fedu,Mjob,Fjob,Dalc,Walc,absences,G3 FROM student_records")
+    cur = cursor.execute("SELECT Pstatus,Medu,Fedu,Mjob,Fjob,Dalc,Walc,absences,G3 FROM GAYATRI.STUDENT_PROFILES WHERE ROWNUM <= 10")
+
     entries = [dict(Pstatus=row[0], Medu=row[1], Fedu=row[2], Mjob=row[3], Fjob=row[4], Dalc=row[5], Walc=row[6], absences=row[7], G3=row[8]) for row in
                cur.fetchall()]
     return render_template('tables.html', entries=entries)
@@ -64,10 +56,4 @@ if __name__ == "__main__":
 
 
 
-
-# @app.route('/')
-# @app.route('/home')
-# def home():
-#     # return "<html><head><title></title><body>" + s + "</body></html>"
-#     return render_template('Dash.html', entries=s)
 
